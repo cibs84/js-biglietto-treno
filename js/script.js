4ce8e892bce97ba67a1e5d89f2089548716187ef
cibs84 <d.tilesi@gmail.com>
2022-06-01 16:44:06 +0200

    Raccolta dati e creazione variabili

--- js/script.js
+++ js/script.js
@@ -5,3 +5,27 @@
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
+// RACCOLTA DATI E CREAZIONE DELLE VARIABILI
+// Chiedo all'utente di inserire il numero di chilometri che vuole percorrere
+let kmTrip = parseInt( prompt('Inserisci i Km che vuoi percorrere') );
+console.log(kmTrip);
+
+// Chiedo all'utente di inserire la sua età
+let ageUser = parseInt( prompt('Inserisci la tua età') );
+console.log(ageUser);
+
+// Creo variabile per tariffa al kilometro
+const ratePerKm = 0.21;
+console.log(ratePerKm);
+
+// LOGICA
+// Calcolo del prezzo pieno del biglietto moltiplicando kmTrip per ratePerKm
+
+
+// Calcolo dei prezzi scontati in base all'età
+// SE l'età è inferiore a 18 ALLORA sottrai al prezzo piano del biglietto lo sconto del 20%
+// SE l'età è superiore a 65 ALLORA sottrai al prezzo piano del biglietto lo sconto del 40%
+
+
+// STAMPO IL PREZZO CON UN ARROTONDAMENTO AL CENTESIMO DI EURO
+
